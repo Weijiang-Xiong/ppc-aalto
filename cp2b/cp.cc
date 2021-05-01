1b2e65f2ae34dc8d6eda449eac32e193f18caecb
Weijiang-Xiong <weijiangxiong1998@gmail.com>
2021-05-01 22:40:12 +0300

    done openmp assign

--- cp2b/cp.cc
+++ cp2b/cp.cc
@@ -6,5 +6,80 @@ This is the function you need to implement. Quick reference:
 - correlation between rows i and row j has to be stored in result[i + j*ny]
 - only parts with 0 <= j <= i < ny need to be filled
 */
-void correlate(int ny, int nx, const float *data, float *result) {
+#include <cmath>
+#include <vector>
+#include <random>
+
+void correlate(int ny, int nx, const float *data, float *result)
+{
+    const int num_group = 4;            // divide a row into some groups
+    const int outlier = nx % num_group; //the trailing elements that do not make a full group
+
+    std::vector<double> normalized(nx * ny);
+    // double normalized[nx * ny]; // can not use normalized[4000*2000] ... don't know why
+    // each row
+    #pragma omp parallel for // need -fopenmp as compiler input
+    for (int y = 0; y < ny; y++)
+    {
+        // First normalize the input rows so that each row has the arithmetic mean of 0  be careful to do the normalization so that you do not change pairwise correlations.
+        double row_sum = 0.0;
+        double row_square_sum = 0.0;
+        // iterate over columns
+        for (int x = 0; x < nx; x++)
+        {
+            row_sum += data[x + y * nx];
+        }
+        double rwo_avg = row_sum / nx;
+        for (int x = 0; x < nx; x++)
+        {
+            double item = data[x + y * nx] - rwo_avg;
+            normalized[x + y * nx] = item;
+            row_square_sum += pow(item, 2);
+        }
+        // Then normalize the input rows so that for each row the sum of the squares of the elements is 1 — again, be careful to do the normalization so that you do not change pairwise correlations.
+        double root_square_sum = sqrt(row_square_sum);
+        for (int x = 0; x < nx; x++)
+        {
+            normalized[x + y * nx] /= root_square_sum;
+        }
+    }
+    // Let X be the normalized input matrix.
+    // Calculate the (upper triangle of the) matrix product Y = XX.T.
+    // row id in XX.T
+    #pragma omp parallel for // need -fopenmp as compiler input
+    for (int row = 0; row < ny; row++)
+    {
+        // column id in XX.T
+        #pragma omp parallel for // need -fopenmp as compiler input
+        for (int col = row; col < ny; col++)
+        {
+            // vector product of each element
+            // actually the row th row in X times the col th row in X
+            // Speed up the inner product
+            // split the vector into nx / num_group blocks, each block contains num_group items
+            // the gid-th item belongs to the gid-th group, and sum the product by group
+            double inner_prod{0};
+            double group_sum[num_group]{0};
+
+            for (int bid = 0; bid < nx / num_group; bid++) // block id
+            {
+                for (int gid = 0; gid < num_group; gid++) // group id
+                {
+                    group_sum[gid] += normalized[bid * num_group + gid + row * nx] * normalized[bid * num_group + gid + col * nx];
+                }
+            }
+            // now sum up the trailing items
+            for (int tid = 0; tid < outlier; tid++)
+            {
+                inner_prod += normalized[(nx - outlier) + tid + row * nx] * normalized[(nx - outlier) + tid + col * nx];
+            }
+            // sum up the group sums
+            for (auto &sum : group_sum)
+            {
+                inner_prod += sum;
+            }
+            
+            result[row * ny + col] = float(inner_prod);
+        }
+    }
 }
--- mf2/mf.cc
+++ mf2/mf.cc
@@ -7,5 +7,51 @@ This is the function you need to implement. Quick reference:
   max(x-hx, 0) <= a < min(x+hx+1, nx), max(y-hy, 0) <= b < min(y+hy+1, ny)
   in out[x + y*nx].
 */
-void mf(int ny, int nx, int hy, int hx, const float *in, float *out) {
+// for MF9a please see Prof. Suomela's paper https://arxiv.org/pdf/1406.1717.pdf
+// and http://www.shellandslate.com/fastmedian.html
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
+{
+	// replace a pixel with the average value in a small window
+	#pragma omp parallel for // need -fopenmp as compiler input
+	for (int y = 0; y < ny; y++)
+	{
+		#pragma omp parallel for // need -fopenmp as compiler input
+		for (int x = 0; x < nx; x++)
+		{
+			int a_start = std::max(x - hx, 0);
+			int a_end = std::min(x + hx + 1, nx);
+			int b_start = std::max(y - hy, 0);
+			int b_end = std::min(y + hy + 1, ny);
+
+			// use a vector container, learned from cp1 assignment ... do not use array...
+			std::vector<double> window((a_end - a_start) * (b_end - b_start));
+			window.clear(); // Erases all the elements.
+
+			for (int b = b_start; b < b_end; b++)
+			{
+				for (int a = a_start; a < a_end; a++)
+				{
+					window.push_back(in[a + b * nx]);
+				}
+			}
+
+			int median_idx = window.size() / 2; // the k+1 th element no matter n = 2k or n=2k+1
+			std::nth_element(window.begin(), window.begin() + median_idx, window.end());
+			double median_cand = window[median_idx];
+
+			if (window.size() % 2 == 0) // even number n = 2k, median_idx=k, corresponds to k+1 th element
+			{
+				std::nth_element(window.begin(), window.begin() + median_idx - 1, window.end());
+				out[x + y * nx] = 0.5 * (window[median_idx - 1] + median_cand);
+			}
+			else // odd number 2k+1, median_idx=k
+			{
+				out[x + y * nx] = median_cand;
+			}
+		}
+	}
 }
