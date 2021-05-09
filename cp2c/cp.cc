022cd703ad540823d62b52850952fad6472fe25c
Weijiang-Xiong <weijiangxiong1998@gmail.com>
2021-05-09 14:58:27 +0300

    no nested openmp

--- cp2c/cp.cc
+++ cp2c/cp.cc
@@ -95,7 +95,6 @@ void correlate(int ny, int nx, const float *data, float *result)
     for (int row = 0; row < ny; row++)
     {
         // column id in XX.T
-        // #pragma omp parallel for // need -fopenmp as compiler input
         for (int col = row; col < ny; col++)
         {
             // vector product of each element
