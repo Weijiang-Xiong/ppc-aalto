5fe990be00ccc319cb614a7036ad6302c1d14c8d
Weijiang-Xiong <weijiangxiong1998@gmail.com>
2021-05-27 17:19:36 +0300

    minor improvement

--- is4/is.cc
+++ is4/is.cc
@@ -55,7 +55,7 @@ Result segment(int ny, int nx, const float *data) {
     double4_t* vectorized = double4_alloc(numel);
     double4_t* img_sum = double4_alloc(numel + nx + ny + 1); // size (ny+1, nx+1)
 
-    auto start = std::chrono::system_clock::now();
+    // auto start = std::chrono::system_clock::now();
     #pragma omp parallel for
     for (int y = 0; y < ny; y++)
     {
@@ -70,11 +70,11 @@ Result segment(int ny, int nx, const float *data) {
             vectorized[position_1d][2] = data[3*position_1d + 2];
         }
     }
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::cout << "padding and vectorize: " << elapsed_seconds.count() << "s\n";
+    // auto end = std::chrono::system_clock::now();
+    // std::chrono::duration<double> elapsed_seconds = end-start;
+    // std::cout << "padding and vectorize: " << elapsed_seconds.count() << "s\n";
 
-    start = std::chrono::system_clock::now();
+    // start = std::chrono::system_clock::now();
     #pragma omp parallel for
     for (int y = 0; y <= ny; y++)
     {
@@ -98,21 +98,23 @@ Result segment(int ny, int nx, const float *data) {
             img_sum[x + (nx+1)*y] = img_sum[x-1 + (nx+1)*y] + img_sum[x + (nx+1)*(y-1)] - img_sum[(x-1)+(nx+1)*(y-1)] + vectorized[(x-1) + nx*(y-1)];
         }
     }
+
     double4_t sum_all = img_sum[nx + (nx+1)*ny];
 
-    end = std::chrono::system_clock::now();
-    elapsed_seconds = end-start;
-    std::cout << "calculating sum: " << elapsed_seconds.count() << "s\n";
+    // end = std::chrono::system_clock::now();
+    // elapsed_seconds = end-start;
+    // std::cout << "calculating sum: " << elapsed_seconds.count() << "s\n";
 
-    start = std::chrono::system_clock::now();
+    // start = std::chrono::system_clock::now();
 
     double overall_best{HUGE_NEGATIVE};
-    int x0_bst{0}, y0_bst{0}, x1_bst{0}, y1_bst{0}; //best coordinate in a thread
+    int x0_bst{0}, y0_bst{0}, x1_bst{0}, y1_bst{0}, w_bst{0}, h_bst{0}; //best coordinate in a thread
     #pragma omp parallel
     {   
         // local initialization keep track of the smallest loss, or largest negative loss. 
         double thread_best{HUGE_NEGATIVE}; // the best loss of this thread
-        int x0_thr{0}, y0_thr{0}, x1_thr{0}, y1_thr{0}; //best coordinate in a thread
+        double window_best{HUGE_NEGATIVE}; // the window size of the best loss in this thread
+        int w_win{0}, h_win{0}; // best h and w for a window
         // individual jobs for each thread
         // use dynamic because different window size means different amount of work
         #pragma omp for schedule(dynamic) 
@@ -142,15 +144,17 @@ Result segment(int ny, int nx, const float *data) {
                         // gcc will prepare the coefficient outside the 2 inner most loops
                         double4_t neg_loss_vec = inv_out * sum_all * sum_all + sum_in_vec * (sum_in_vec * (inv_in+inv_out) - 2.0 * inv_out * sum_all);
                         double neg_loss = neg_loss_vec[0] + neg_loss_vec[1] + neg_loss_vec[2];
-                        if (neg_loss > thread_best)
+                        if (neg_loss > window_best)
                         {
-                            thread_best = neg_loss;
-                            x0_thr = x0;
-                            y0_thr = y0;
-                            x1_thr = x1;
-                            y1_thr = y1;
+                            window_best = neg_loss;
                         }
                     }
+                }
+                if (window_best > thread_best)
+                {
+                    thread_best = window_best;
+                    w_win = w;
+                    h_win = h;
                 }
             }
         }
@@ -160,19 +164,42 @@ Result segment(int ny, int nx, const float *data) {
             if (thread_best > overall_best)
             {
                 overall_best = thread_best;
-                x0_bst = x0_thr;
-                y0_bst = y0_thr;
-                x1_bst = x1_thr;
-                y1_bst = y1_thr;
+                w_bst = w_win;
+                h_bst = h_win;
+            }
+        }
+    }
+
+    int numel_in = h_bst*w_bst;
+    int numel_out = numel - numel_in;
+    double inv_in = static_cast<double>(1) / numel_in;
+    double inv_out = static_cast<double>(1) / numel_out;
+    for (int y0 = 0; y0 <= ny-h_bst; y0++)
+    {
+        // put x in the inner most, so memory access will be continuous
+        for (int x0 = 0; x0 <= nx-w_bst; x0++) 
+        {
+            int x1 = x0 + w_bst;
+            int y1 = y0 + h_bst;
+            // S_in = S(x1, y1) - S(x0, y1) - S(x1, y0) + S(x0, y0)
+            double4_t sum_in_vec = img_sum[x1+(nx+1)*y1] - img_sum[x0+(nx+1)*y1] - img_sum[x1+(nx+1)*y0] + img_sum[x0+(nx+1)*y0];
+            double4_t neg_loss_vec = inv_out * sum_all * sum_all + sum_in_vec * (sum_in_vec * (inv_in+inv_out) - 2.0 * inv_out * sum_all);
+            double neg_loss = neg_loss_vec[0] + neg_loss_vec[1] + neg_loss_vec[2];
+            if (neg_loss >= overall_best)
+            {
+                x0_bst = x0;
+                y0_bst = y0;
+                x1_bst = x1;
+                y1_bst = y1;
             }
         }
     }
 
     // calculate final result with best value
-    int numel_in = (y1_bst - y0_bst)*(x1_bst - x0_bst);
-    int numel_out = numel - numel_in;
-    double inv_in = static_cast<double>(1) / numel_in;
-    double inv_out = static_cast<double>(1) / numel_out;
+    numel_in = (y1_bst - y0_bst)*(x1_bst - x0_bst);
+    numel_out = numel - numel_in;
+    inv_in = static_cast<double>(1) / numel_in;
+    inv_out = static_cast<double>(1) / numel_out;
     double4_t sum_in_vec = img_sum[x1_bst+(nx+1)*y1_bst] - img_sum[x0_bst+(nx+1)*y1_bst] 
                          - img_sum[x1_bst+(nx+1)*y0_bst] + img_sum[x0_bst+(nx+1)*y0_bst];
     double4_t sum_out_vec = sum_all - sum_in_vec;
@@ -184,12 +211,12 @@ Result segment(int ny, int nx, const float *data) {
                  {float(sum_out_vec[0]), float(sum_out_vec[1]), float(sum_out_vec[2])},
                  {float(sum_in_vec[0]), float(sum_in_vec[1]), float(sum_in_vec[2])}};
 
-    end = std::chrono::system_clock::now();
-    elapsed_seconds = end-start;
-    std::cout << "iterating over all possible places: " << elapsed_seconds.count() << "s\n";
+    // end = std::chrono::system_clock::now();
+    // elapsed_seconds = end-start;
+    // std::cout << "iterating over all possible places: " << elapsed_seconds.count() << "s\n";
 
     free(vectorized);
     free(img_sum);
-    return result;
 
+    return result;
 }
